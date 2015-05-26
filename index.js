a820bfc7f64bb43e52e66f784af7b650056ef633
tunnckoCore <mameto_100@mail.bg>
2015-05-26 12:09:17 +0300

    implement

---
+++ index.js
@@ -0,0 +1,51 @@
+/*!
+ * acorn-extract-comments <https://github.com/tunnckoCore/acorn-extract-comments>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var acorn = require('acorn')
+
+module.exports = function acornExtractComments (input, opts) {
+  if (typeof input !== 'string') {
+    throw new TypeError('acorn-extract-comments expect a string')
+  }
+
+  opts = typeof opts === 'object' ? opts : {}
+  opts.ecmaVersion = opts.ecmaVersion || 6
+  opts.locations = true
+
+  var comments = opts.onComment = []
+  var corns = acorn.parse(input, opts)
+  var lines = input.split('\n')
+  var result = {}
+
+  comments = comments.filter(function (comment, i) {
+    if (comment.type === 'Block' && comment.value.charAt(0) !== '!') {
+      var commentEnd = comment.loc.end.line
+      var codeStart = lines[commentEnd]
+
+      if (codeStart.indexOf('  ') === 0 && !opts.nested) {
+        return null
+      }
+
+      if (!codeStart.trim()) {
+        commentEnd = commentEnd + 1
+        codeStart = lines[commentEnd]
+      }
+
+      comment.after = codeStart
+      return comment
+    }
+  })
+
+  if (opts.ast) {
+    result.ast = corns
+  }
+  result.comments = comments
+
+  return result
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+/*!
+ * acorn-extract-comments <https://github.com/tunnckoCore/acorn-extract-comments>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var fs = require('fs')
+var test = require('assertit')
+var acornExtractComments = require('./index')
+
+test('acorn-extract-comments:', function () {
+  test('should extract comments to `.comments` prop (without options)', function (done) {
+    var input = fs.readFileSync('./fixture.js', 'utf8')
+    var actual = acornExtractComments(input)
+
+    test.equal(typeof actual, 'object')
+    test.ok(!actual.ast)
+    test.ok(Array.isArray(actual.comments))
+    done()
+  })
+  test('should pass AST to `.ast` prop (when `opts.ast:true`)', function (done) {
+    var input = fs.readFileSync('./fixture.js', 'utf8')
+    var actual = acornExtractComments(input, {ast: true})
+
+    test.equal(typeof actual, 'object')
+    test.equal(typeof actual.ast, 'object')
+    test.ok(Array.isArray(actual.comments))
+    done()
+  })
+})
